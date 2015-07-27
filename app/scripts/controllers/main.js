c7fdee4a618e358d3e3761f3dc67cf8a47c9e721
Santiago Ferreira <san650@gmail.com>
2015-07-27 08:33:59 -0300

    Generated app with reading list template and styles

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name readingListApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the readingListApp
+ */
+angular.module('readingListApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
